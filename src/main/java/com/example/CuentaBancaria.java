5a84dfa8991a9e340454042ff80a27b3c6e3ccb2
andreyhernandezpatiño <andreyhernandezpatino8@gmail.com>
2026-08-21 16:53:36 -0500

    ejercicio2

--- src/main/java/com/example/CuentaBancaria.java
+++ src/main/java/com/example/CuentaBancaria.java
@@ -1,5 +1,40 @@
 package com.example;
 
 public class CuentaBancaria {
+    private String titular ;
+    private double saldo ;
 
-}
+    public CuentaBancaria (String titular, double saldoinicial){
+        this.titular = titular;
+        if (saldoinicial <0){
+            this.saldo =0;
+        }else {
+            this.saldo = saldoinicial;
+        }
+        }
+    public String gettitular(){
+        return titular;
+    }
+    public void settitular (String titular){
+        this.titular = titular;
+    }
+    public double getsaldo(){
+        return saldo;
+    }
+    public void depositar (double cantidad){
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }else{
+            System.out.println("la cantidad a depositar debe ser mayor a 0");
+        }
+    }
+    public void retirar(double cantidad){
+        if (cantidad <= 0) {
+            System.out.println("la cantidad a retirar debe ser mayor a 0");
+        }else if (cantidad > saldo){
+            System.out.println("fondos insuficientes");
+        }else {
+            saldo -= cantidad;
+        }
+    }
+    }
